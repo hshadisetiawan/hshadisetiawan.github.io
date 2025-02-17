a01878cea7bc3aff423721cee1319842ffa43dc9
Hadi Setiawan <hs.hadisetiawan@gmail.com>
2025-02-17 20:12:04 +0700

    Add files via upload

---
+++ assets/js/ads.js
@@ -0,0 +1,5 @@
+// https://dummyimage.com/320x50/000000/ffffff.png&text=320x50
+document.getElementById("320x50=1").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=2").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=3").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=4").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
---
+++ assets/js/page.js
@@ -0,0 +1,21 @@
+function Home_Page() {
+	location.replace('//hshadisetiawan.github.io')
+}
+function Content_Page() {
+	location.replace('//hshadisetiawan.github.io/content.html')
+}
+function Portofolio_Page() {
+	location.replace('//hshadisetiawan.github.io/portofolio.html')
+}
+function Services_Page() {
+	location.replace('//hshadisetiawan.github.io/services.html')
+}
+function Disclaimer_Page() {
+	location.replace('//hshadisetiawan.github.io/disclaimer.html')
+}
+function PrivacyPolicy_Page() {
+	location.replace('//hshadisetiawan.github.io/privacy-policy.html')
+}
+function CookiePolicy_Page() {
+	location.replace('//hshadisetiawan.github.io/cookie-policy.html')
+}
