d965b86b861d07c5719ed6a6fd603cbf64b4c974
Hadi Setiawan <hs.hadisetiawan@gmail.com>
2025-08-23 19:07:11 +0700

    Update ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,5 +1,21 @@
 // https://dummyimage.com/320x50/000000/ffffff.png&text=320x50
+
+
+
 document.getElementById("320x50=1").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=1").href = "/";
+
+
+
 document.getElementById("320x50=2").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=1").href = "/";
+
+
+
 document.getElementById("320x50=3").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=1").href = "/";
+
+
+
 document.getElementById("320x50=4").src = "https://dummyimage.com/320x50/000000/ffffff.png&text=320x50";
+document.getElementById("320x50=1").href = "/";
